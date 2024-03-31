811d8748bf5f26966ce5abf5122cd4ff6dc61cca
wangrugeng <wangrugeng@bytedance.com>
2024-03-31 16:46:27 +0800

    提交

---
+++ src/main/java/com/hanyuling/algorithm/dataStructure/FindTargetNode.java
@@ -0,0 +1,32 @@
+package com.hanyuling.algorithm.dataStructure;
+
+import com.hanyuling.algorithm.dataStructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: wrg
+ * @date: 2024/3/28 20:44
+ */
+public class FindTargetNode {
+
+    public static void main(String[] args) {
+        FindTargetNode find = new FindTargetNode();
+    }
+
+    public int findTargetNode(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
+        return list.get(list.size() - k);
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, list);
+        list.add(root.val);
+        dfs(root.right, list);
+    }
+}
---
+++ src/test/java/TestA.java
@@ -0,0 +1,50 @@
+import com.hanyuling.algorithm.linklist.FlipsList;
+import com.hanyuling.algorithm.linklist.ListNode;
+
+import java.util.List;
+
+/**
+ * @author: wrg
+ * @date: 2024/3/27 15:25
+ */
+public class TestA {
+    //单链表 每k个节点逆序
+    // 结尾不满足k个节点 不逆序 返回新的列表
+    // 12345678 32165478 3 O(n) 空间复杂度
+
+    public static ListNode reversk(ListNode head, int k) {
+        ListNode hair = new ListNode(0, head);
+        ListNode pre = hair;
+        while (head != null) {
+            ListNode tail = pre;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] listNodes = myReverse(head, tail);
+            head = listNodes[0];
+            tail = listNodes[1];
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+        return hair.next;
+    }
+
+    private static ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode tmp = p.next;
+            p.next = prev;
+            prev = p;
+            p = tmp;
+        }
+        return new ListNode[]{tail, head};
+    }
+
+}
